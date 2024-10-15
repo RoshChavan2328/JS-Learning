63a778e801e69aabccd5bdf91796eb1fda1768f6
RoshChavan2328 <chavanroshan2328@gmail.com>
2024-10-15 09:56:58 +0000

    Update

---
+++ JsBasicCodes/DataTypes.js
@@ -0,0 +1,25 @@
+/* Primitive datatypes*/
+
+let name="Roshan";        //String
+let age=24;               //Number
+let isGraduated=true;     //Boolean
+let address;              //undefined
+let adharNo=null;        //Null   //type=object
+                         //BigInt
+                         //symbol
+
+console.table([name,age,isGraduated,address,adharNo],[typeof(name),typeof(age),typeof(isGraduated),typeof(address),typeof(adharNo)]);
+console.table([
+    { Property: "Name", Value: name, Type: typeof(name) },
+    { Property: "Age", Value: age, Type: typeof(age) },
+    { Property: "Is Graduated", Value: isGraduated, Type: typeof(isGraduated) },
+    { Property: "Address", Value: address, Type: typeof(address) },
+    { Property: "Adhar No", Value: adharNo, Type: typeof(adharNo) }
+  ]);
+
+  console.table([
+    {Property : "Name", value:name,Type:typeof(name)},
+    {Property : "age", value : age,Type:typeof(age)}]
+  )
+
+  /*Non Primitive datatypes  */
---
+++ JsBasicCodes/Variables.js
@@ -0,0 +1,7 @@
+const pi=3.14;    //constant declaration ,cannot redeclare and reassign 
+var globalVariable ="variables declare with var can be Redeclare and reassign";
+let blockVariable ="variables declare with let cannot be Redeclare but can  reassign";
+variable = 10;
+
+console.table([pi,globalVariable,blockVariable,variable]);
+
